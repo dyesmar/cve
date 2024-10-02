dca5721b49c60de83b7f2314a0b6aa6b8f901fe2
Ramsey Dow <ramsey@casaba.com>
2024-10-02 12:46:37 -0700

    Add test for CVE.String() method

--- cve_test.go
+++ cve_test.go
@@ -135,3 +135,16 @@ func TestInvalid(t *testing.T) {
 		})
 	}
 }
+
+// TestInvalid tests Stringer conformance. There is no real way the
+// String method can fail since CVE's year and sequence fields were
+// rendered unexported. (The New and Parse methods handle all validation
+// up front.) Nonetheless, we will give it a go just to say we tested it.
+func TestString(t *testing.T) {
+	want := "CVE-2014-9999999"
+	cve, err := Parse(want)
+	s := cve.String()
+	if want != s || err != nil {
+		t.Fatalf(`String() = %q, %v, want match for %#q, nil`, s, err, want)
+	}
+}
